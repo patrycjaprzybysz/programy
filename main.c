80e63a4c074de2d8e955d51869cdfc788506766e
patrycjaprzybysz <100605325+patrycjaprzybysz@users.noreply.github.com>
2022-03-07 17:18:20 +0100

    Add files via upload
    sortowanie bąbelkowe

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello World!-nazywam sie Patrycja Przybysz. Naleze do grupy: 215IC_B1"); //wyœwietlenie napisu
-    return 0;
+ void bubbleSort(int tablica[]){
+    int length=15;
+    for(int i=0;i<length;i++){
+        for(int j=1;j<length;j++){
+            if(tablica[j-1]>tablica[j]){
 
+               int a= tablica[j-1];
+               tablica[j-1]=tablica[j];
+               tablica[j]=a;
+
+            }
+        }
+    }
+
+
+}
+int main(){
+int tablica[15]={5,2,3,4,1,8,6,0,3,7,4,1,9,2,5};
+    printf("Wyswietlenie nieposortowanej tablicy\n");
+     for(int i=0;i<15;i++){
+    printf("%d", tablica[i]);
+     }
+     printf("\nWyswietlenie posortowanej tablicy\n");
+    bubbleSort(tablica);
+    for(int i=0;i<15;i++){
+        printf("%d",tablica[i]);
+    }
+return 0;
 }
