e23bc671fe18a565f65273840434acb5943fc221
patrycjaprzybysz <100605325+patrycjaprzybysz@users.noreply.github.com>
2022-03-07 17:24:57 +0100

    Add files via upload
    rozgrzewka

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello World!-nazywam sie Patrycja Przybysz. Naleze do grupy: 215IC_B1"); //wyœwietlenie napisu
+    return 0;
+
+}
