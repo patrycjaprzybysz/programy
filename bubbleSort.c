1628ea9bf7fe9ff8a72d3544dee0cc32f92308a4
patrycjaprzybysz <100605325+patrycjaprzybysz@users.noreply.github.com>
2022-03-07 19:43:59 +0100

    Update bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- void bubbleSort(int tablica[]){//stworzenie funkcji sortuj¹cej
-    int length=15; //wprowadzenie zmiennej oznaczjacej d³ugoœæ tablicy
+ void bubbleSort(int tablica[]){//stworzenie funkcji sortujÄ…cej
+    int length=15; //wprowadzenie zmiennej oznaczjacej dÅ‚ugoÅ›Ä‡ tablicy
     for(int i=0;i<length;i++){ //przejscie przez kolejne elementy tablicy
-        for(int j=1;j<length;j++){//pêtla s³u¿¹ca do porównania elementów tablicy
-            if(tablica[j-1]>tablica[j]){//porownanie kolejnych elelmentow tablicy
-                                        //(jeœli spelniony jest warunek wykonuje sie kod 11-13, jesli nie element zostaje na swoim miejscu)
-               // zamienienie miejscami elementów tablicy (jeœli pierwszy jest wiekszy od drugiego)
+        for(int j=1;j<length;j++){//pÄ™tla sÅ‚uÅ¼Ä…ca do porÃ³wnania elementÃ³w tablicy
+            if(tablica[j-1]>tablica[j]){//porownanie kolejnych sÄ…siadujÄ…cych elelmentow tablicy
+                                        //(jeÅ›li spelniony jest warunek wykonuje sie kod 11-13, jesli nie element zostaje na swoim miejscu)
+               // zamienienie miejscami elementÃ³w tablicy (jeÅ›li pierwszy jest wiekszy od drugiego)
                int a= tablica[j-1];
                tablica[j-1]=tablica[j];
                tablica[j]=a;
@@ -25,7 +25,7 @@ int tablica[15]={5,2,3,4,1,8,6,0,3,7,4,1,9,2,5};//
     printf("%d", tablica[i]);
      }
      printf("\nWyswietlenie posortowanej tablicy\n");
-    bubbleSort(tablica);//wywyo³anie funkcji sortujacej tablice
+    bubbleSort(tablica);//wywyoÅ‚anie funkcji sortujacej tablice
     for(int i=0;i<15;i++){//wyswietlenie posortowanej tablicy
         printf("%d",tablica[i]);
     }
